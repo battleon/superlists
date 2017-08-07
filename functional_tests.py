2471fe6a6b71c2367c951e26243ecd5113a83ebd
Battleon <2277850@qq.com>
2017-08-07 23:24:40 +0800

    单元测试调整，第60页完成。

--- functional_tests.py
+++ functional_tests.py
@@ -36,15 +36,21 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 他输入了“ Use peacock feathers to make a fly (使用孔雀羽毛做假蝇)”
         # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新,他的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
         # 伊迪丝想知道这个网站是否会记住他的清单
 
         # 他看到网站为他生成了一个唯一的URL
@@ -52,6 +58,7 @@ class NewVisitorTest(unittest.TestCase):
         # 他访问那个URL,发现他的待办事项列表还在
 
         # 他很满意,去睡觉了
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
