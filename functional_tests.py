aff5b4819bad2d0311407e0d47799a45e80e9625
Battleon <2277850@qq.com>
2017-08-08 10:50:10 +0800

    重定向页面，完成数据在模板页面上的展示。

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -12,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 小明听说了一个很不错了To-Do应用网站
@@ -33,25 +39,23 @@ class NewVisitorTest(unittest.TestCase):
         # 他按回车键后,页面更新了
         # 待办事项表格中显示了“ 1: Buy peacock feathers ”
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 他输入了“ Use peacock feathers to make a fly (使用孔雀羽毛做假蝇)”
-        # 伊迪丝做事很有条理
+        # 小明做事很有条理
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新,他的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
-        # 伊迪丝想知道这个网站是否会记住他的清单
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # 小明想知道这个网站是否会记住他的清单
 
         # 他看到网站为他生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
@@ -59,6 +63,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # 他很满意,去睡觉了
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main()
