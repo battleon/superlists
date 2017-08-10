17d43fe5785d6359f7173761d6c7aedbffa1418b
Battleon <2277850@qq.com>
2017-08-10 16:52:11 +0800

    添加通过POST请求新建清单所需的URL。

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page
-from lists.views import view_list
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
     url(r'^$', home_page),
     url(r'^lists/the-only-list-in-the-world/$', view_list),
+    url(r'^lists/new$', new_list),
     # url(r'^admin/', include(admin.site.urls)),
 ]
