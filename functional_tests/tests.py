25e8330fd85c0df770540e365e87a153a16c25a5
Battleon <2277850@qq.com>
2017-08-10 10:26:29 +0800

    变更功能测试模式，调用Django自带测试模块进行。

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """docstring for NewVisitorTest"""
 
     def setUp(self):
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 小明听说了一个很不错了To-Do应用网站
         # 他去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 他注意到网页的标题和头部都包含“ To-Do ”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -63,7 +63,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # 他很满意,去睡觉了
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main()
