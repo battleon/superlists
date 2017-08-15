6646769265a7c3a02b15195729249a4be0dea6d9
Battleon <2277850@qq.com>
2017-08-15 11:57:25 +0800

    使用Bootstrap 拓展完善布局。

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """docstring for NewVisitorTest"""
 
     def setUp(self):
@@ -89,6 +89,6 @@ class NewVisitorTest(LiveServerTestCase):
         # 她看到输入框完美地居中显示
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing\n')
+        inputbox.send_keys('testing')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
