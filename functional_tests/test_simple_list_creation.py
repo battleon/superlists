e3710c54665db1a8301f37e2b29c288f4c5adcce
Battleon <2277850@qq.com>
2017-08-18 10:52:11 +0800

    将功能测试拆分成独立的测试模块文件。

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,24 +1,9 @@
-import sys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    """docstring for NewVisitorTest"""
-
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说了一个很不错了To-Do应用网站
         # 她去看了这个应用的首页
@@ -82,14 +67,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
         # 两人都很满意，去睡觉了
-
-    def test_layout_and_styling(self):
-        # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-        # 她看到输入框完美地居中显示
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
